0cbce928678e4f82a84bfd20221f53162c8e4e82
전현우 <soshot22@naver.com>
2022-09-07 08:52:29 +0900

    feat : 로그인 화면 구성
    로그인 화면 구성
    Close #S07P22E101-11

--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Login페이지</div>
+  <div class="container">
+    <div class="row">
+      <img src="" alt="대표 엽서 이미지">
+      <v-divider vertical />
+    <div class="container">
+      <button type="button" class="btn btn-primary">버튼</button>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
