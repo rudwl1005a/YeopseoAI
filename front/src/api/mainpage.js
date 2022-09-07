e49dc56c7e6c72b6fb48e131876714275d889663
박정현 <kevin123488@naver.com>
2022-09-07 16:50:34 +0900

    feat : 메인페이지 화면 구성, 메인페이지 axios 추가
    완료했습니다

--- front/src/api/mainpage.js
+++ front/src/api/mainpage.js
@@ -4,6 +4,7 @@ const api = apiInstance();
 // 메인페이지에서 필요한 요청 사항은?
 // 인기 엽서 목록 대략 10개
 // 인기 재단 목록 대략 10개
+// 인기 엽서 클릭하면 팝업으로 해당 엽서를 좋아하는 인원과 작성자 등의 정보를 받아야 함
 
 async function getFamousLetter(success, fail) { // 인기 편지 목록
     await api.get(`/api/postcards/top-list`).then(success).catch(fail);
@@ -13,7 +14,22 @@ async function getFamousFoundation(success, fail) { // 인기 재단 목록
     await api.get(`/api/foundations/list/top`).then(success).catch(fail);
 }
 
+async function likeLetter(postcardSeq, userSeq, success, fail) {
+    await api.post(`/api/postcards/${postcardSeq}/${userSeq}`).then(success).catch(fail);
+}
+
+async function dislikeLetter(postcardSeq, userSeq, success, fail) {
+    await api.delete(`/api/postcards/${postcardSeq}/${userSeq}`).then(success).catch(fail);
+}
+
+async function letterDetail(postcardSeq, success, fail) {
+    await api.get(`/api/postcards/${postcardSeq}`).then(success).catch(fail);
+}
+
 export {
     getFamousLetter,
     getFamousFoundation,
+    likeLetter,
+    dislikeLetter,
+    letterDetail,
 }
