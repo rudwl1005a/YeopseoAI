31ab2b15755df78dcda42fda0d5436529359035d
박정현 <kevin123488@naver.com>
2022-09-13 13:51:56 +0900

    feat : 메인페이지구성초안
    메인페이지구성 초안입니다

--- front/src/views/SearchView.vue
+++ front/src/views/SearchView.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>SearchResult</div>
-  </template>
-  
-  <script>
-  export default {
-    name:"SearchView",
+  <div>SearchResult</div>
+</template>
+
+<script>
+export default {
+  name:"SearchView",
+  data() {
+    return {
+      searchResult: [], // 검색 결과가 담길 곳. store에 세팅된 검색 결과를 가져오자
+    }
   }
-  </script>
-  
-  <style>
-  
-  </style>
+}
+</script>
+
+<style>
+
+</style>
