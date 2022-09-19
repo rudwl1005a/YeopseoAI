3efe5dd584f5797ba12591223c58b7c099adb438
jimin6206 <jimin6206@gmail.com>
2022-09-19 16:57:12 +0900

    test : swagger test
    swagger test

--- back/src/main/java/com/ssafy/config/SwaggerConfig.java
+++ back/src/main/java/com/ssafy/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,6 +17,7 @@ import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -28,7 +31,9 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        Server testServer = new Server("test","https://j7e101.p.ssafy.io","for testing", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+                .servers(testServer)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
