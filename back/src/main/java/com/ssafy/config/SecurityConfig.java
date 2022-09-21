5bf5d2b897e2852894f9f3b9f08d579bba7db5d5
최년우 <chl1380@naver.com>
2022-09-21 10:02:40 +0900

    feat : swagger uri 설정 삭제

--- back/src/main/java/com/ssafy/config/SecurityConfig.java
+++ back/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -65,11 +65,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	        	    .anyRequest().permitAll()
                 .and().cors();
     }
-     @Override
-    public void configure(WebSecurity web) {
-            
-    web
-        .ignoring()
-        .mvcMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs","/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}");
-    }
 }
