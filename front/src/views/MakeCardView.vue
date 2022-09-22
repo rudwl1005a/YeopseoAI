baceb770511a04571694a6e471b09e7bc023d726
박정현 <kevin123488@naver.com>
2022-09-22 16:37:15 +0900

    feat : 그림판 이미지 변환, formdata

--- front/src/views/MakeCardView.vue
+++ front/src/views/MakeCardView.vue
@@ -1,13 +1,19 @@
 <template>
     <div>MakeCard</div>
-  </template>
-  
-  <script>
-  export default {
-    name:"MakeCardView",
+    <div @click="goCanvas">엽서 디자인 화면으로</div>
+</template>
+
+<script>
+export default {
+  name:"MakeCardView",
+  methods: {
+    goCanvas() {
+      this.$router.push("/canvasTest");
+    }
   }
-  </script>
-  
-  <style>
-  
-  </style>
+}
+</script>
+
+<style>
+
+</style>
